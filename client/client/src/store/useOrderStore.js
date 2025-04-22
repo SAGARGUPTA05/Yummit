b02be52c5735172654b00417797773de55a06660
Sagar Gupta <168528321+SAGARGUPTA05@users.noreply.github.com>
2025-04-22 14:17:40 +0530

    Update useOrderStore.js

--- client/client/src/store/useOrderStore.js
+++ client/client/src/store/useOrderStore.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
-const API_END_POINT = "http://localhost:3000/api/v1/order";
+const API_END_POINT = "https://yummit.onrender.com/api/v1/order";
 axios.defaults.withCredentials = true;
 export const useOrderStore = create()(
   persist(
